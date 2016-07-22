2da6ef49192933d276a3d41fa82f06e518656bf2
Mikael Arneborn <mikael@arneborn.net>
2016-07-22 15:23:27 -0700

    Added HT.forEach test

--- spec/HierarchicalTimer.spec.js
+++ spec/HierarchicalTimer.spec.js
@@ -62,6 +62,38 @@ describe("HierarchicalTimer", function () {
         });
     });
 
+    describe(".forEach", function () {
+
+        let forEachSpy;
+
+        beforeEach(function () {
+            forEachSpy = jasmine.createSpy('forEach');
+        });
+
+        it("should call each of the children in order", function () {
+            let events = [];
+            timer.on('stop', function (ev) {
+                events.push(ev);
+            });
+
+            timer.start(1);
+            timer.stop();
+            timer.start(2);
+            timer.stop();
+
+            timer.forEach(forEachSpy);
+
+            expect(forEachSpy.calls.argsFor(0)[0]).toBe(events[0], '0-event');
+            expect(forEachSpy.calls.argsFor(1)[0]).toBe(events[1], '1-event');
+            expect(forEachSpy.calls.argsFor(0)[1]).toBe(0, '0-index');
+            expect(forEachSpy.calls.argsFor(1)[1]).toBe(1, '1-index');
+            expect(forEachSpy.calls.argsFor(0)[2]).toBe(timer, '0-parent');
+            expect(forEachSpy.calls.argsFor(1)[2]).toBe(timer, '1-parent');
+            expect(forEachSpy.calls.argsFor(0)[3]).toEqual(events, '0-siblings');
+            expect(forEachSpy.calls.argsFor(1)[3]).toEqual(events, '1-siblings');
+        });
+    });
+
     describe("Emit errors:", function () {
         let errSpy;
 
